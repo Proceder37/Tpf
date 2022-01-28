9e05faf03fc1a0181e3ccec186a2ba8055a34f52
Proceder37 <patryk.kaczor611@gmail.com>
2022-01-28 19:06:32 +0100

    articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ArticleComponent } from './article/article.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { ShotsComponent } from './shots/shots.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'drinks', component: DrinksComponent },
+  { path: 'shots', component: ShotsComponent },
+  { path: 'cocktails', component: CocktailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ArticleComponent } from './article/article.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { ShotsComponent } from './shots/shots.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    ArticleComponent,
+    DrinksComponent,
+    ShotsComponent,
+    CocktailsComponent
   ],
   imports: [
     BrowserModule,
